d1dd122ee46a29cdd7be714a6396f1359aa34f64
Jagadeesh J <jagadeeshj@live.com>
2025-01-24 22:08:01 +0530

    Add tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,22 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `juniorhrc`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `yourdomain`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `pages`,
+        // Path to the directory
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+  ],
 };
